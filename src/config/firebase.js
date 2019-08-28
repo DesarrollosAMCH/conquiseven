e83e3f7c55ecca624cbc1da611ecab44287923da
nfredes <nfredes@grouponlatam.com>
2019-08-28 17:11:59 -0400

    firebase with test env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,9 @@
 export default {
-  apiKey: 'AIzaSyBsItyeUARyecHEKx5kvmCXfhjZUQDMBtM',
-  authDomain: 'conquisevent-prod.firebaseapp.com',
-  databaseURL: 'https://conquisevent-prod.firebaseio.com',
-  projectId: 'conquisevent-prod',
-  storageBucket: 'conquisevent-prod.appspot.com',
-  messagingSenderId: '451623994022',
-  appId: '1:451623994022:web:6a28fce7d0b7a758'
+  apiKey: "AIzaSyDd0NUq8ptQYPeT47NFEOK0DTFN2AFJAzc",
+  authDomain: "conquisevent-test.firebaseapp.com",
+  databaseURL: "https://conquisevent-test.firebaseio.com",
+  projectId: "conquisevent-test",
+  storageBucket: "conquisevent-test.appspot.com",
+  messagingSenderId: "732328802039",
+  appId: "1:732328802039:web:696d214c11eda376"
 }
